40841c49f364e0b09c38d0319793765bacf8dd4e
Diego Machado <machadodiegoh@gmail.com>
2020-08-08 11:16:36 -0300

    Updates

---
+++ precache-manifest.263f636088ea9863fe15c0a9faf84798.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c2861949bef1f233d4e4565f70a787",
+    "url": "/github_explorer/index.html"
+  },
+  {
+    "revision": "eebe9562b10fe6e16c25",
+    "url": "/github_explorer/static/js/2.a39f53a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github_explorer/static/js/2.a39f53a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96dfb883d522ab887069",
+    "url": "/github_explorer/static/js/main.af0ecb98.chunk.js"
+  },
+  {
+    "revision": "d909fb038cc7d9e00687",
+    "url": "/github_explorer/static/js/runtime-main.3daa8093.js"
+  },
+  {
+    "revision": "fe82ec0011d3b27b10f6a2991e6ce6d2",
+    "url": "/github_explorer/static/media/githubLogo.fe82ec00.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github_explorer/static/media/logo.04f6798f.svg"
+  }
+]);
